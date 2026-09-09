d3f5f8b62a6bd433ef304e93072e6259e0b29bef
cursednight774-glitch <cursednight774@gmail.com>
2026-09-09 20:59:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./theme.css"
-import "./app.css"
+import "./App.css"
 import App from "./App.jsx"
 import { loadMe } from "./api.js"
 
